64d0bb8450dd9fa211c7ed5e3d16a5aca4aa968c
unknown <adek236@gmail.com>
2022-12-08 13:28:39 +0100

    Added ejs layout. Login page styled.

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const port = process.env.PORT || 3000
+const expressLayouts = require('express-ejs-layouts');
 
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "public")))
 
+app.use(expressLayouts);
 app.set("view engine", "ejs")
 app.set("views","./views")
 
+
 app.get('/', (req, res) => {
-  return res.render("main.ejs")
+  return res.render("login.ejs")
 })
 
+app.get('/register', (req, res) => {
+  return res.render("register.ejs")
+})
 
 
 
